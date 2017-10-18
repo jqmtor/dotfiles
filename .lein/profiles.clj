57ac9e5cc6210b10471a93fdd57840c83b1c0ed7
Joaquim Torres <quim.r.s.torres@gmail.com>
2017-10-18 21:15:37 +0200

    Add Leiningen configuration

---
+++ .lein/profiles.clj
@@ -0,0 +1,9 @@
+{:user {:plugins      [[refactor-nrepl "2.2.0"]
+                       [cider/cider-nrepl "0.14.0"]
+                       [com.jakemccrary/lein-test-refresh "0.18.0"]
+                       [lein-kibit "0.1.2"]
+                       [lein-environ "1.0.1"]
+                       [io.aviso/pretty "0.1.31"]
+                       [lein-ancient "0.6.10"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [io.aviso/pretty "0.1.31"]]}}
