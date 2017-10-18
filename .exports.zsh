7c731976531614dddb98a6385a7090d07eb43be3
Joaquim Torres <quim.r.s.torres@gmail.com>
2017-10-18 20:58:12 +0200

    Replace exports

---
+++ .exports.zsh
@@ -0,0 +1,32 @@
+# Make vim the default editor.
+export EDITOR='vim';
+
+# set history variables
+export HISTSIZE=32768
+export SAVEHIST=$HISTSIZE
+HISTFILE="$HOME/.zsh_history"
+
+# Prefer US English and use UTF-8.
+export LANG='en_US.UTF-8';
+export LC_ALL='en_US.UTF-8';
+
+# Don’t clear the screen after quitting a manual page.
+export MANPAGER='less -X';
+
+# iTerm
+export TERM="xterm-256color"
+
+# ls command
+export CLICOLOR=1
+export LSCOLORS=ExFxCxDxBxegedabagacad
+
+# grep command
+export GREP_OPTIONS="--color"
+
+# Golang
+# GOPATH is exported in .path instead
+
+# Add GNU utilities man entries
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+export MANPATH="/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH"
+export MANPATH="/usr/local/opt/findutils/libexec/gnuman:$MANPATH"
