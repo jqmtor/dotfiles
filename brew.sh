4d822e0aebf116bd1f79f88fb278d3880ba23da9
Joaquim Torres <quimrstorres@gmail.com>
2017-10-19 17:11:23 +0200

    Install Consolas font

--- brew.sh
+++ brew.sh
@@ -15,4 +15,15 @@ if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
   chsh -s /usr/local/bin/zsh;
 fi;
 
+install_consolas_font () {
+    brew install cabextract
+    cd ~/Downloads
+    mkdir consolas
+    cd consolas
+    curl -O http://download.microsoft.com/download/f/5/a/f5a3df76-d856-4a61-a6bd-722f52a5be26/PowerPointViewer.exe
+    cabextract PowerPointViewer.exe
+    cabextract ppviewer.cab
+    open CONSOLA*.TTF
+}
 
+install_consolas_font
