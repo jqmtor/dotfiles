82ffbb52c95111665061b7aab26a7e113592812f
Joaquim Torres <quimrstorres@gmail.com>
2017-10-21 20:20:00 +0200

    Update Lein profile dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
-{:user {:plugins      [[refactor-nrepl "2.2.0"]
-                       [cider/cider-nrepl "0.14.0"]
+{:user {:plugins      [[refactor-nrepl "2.3.1"]
+                       [cider/cider-nrepl "0.15.1"]
                        [com.jakemccrary/lein-test-refresh "0.18.0"]
-                       [lein-kibit "0.1.2"]
-                       [lein-environ "1.0.1"]
-                       [io.aviso/pretty "0.1.31"]
-                       [lein-ancient "0.6.10"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                       [io.aviso/pretty "0.1.31"]]}}
+                       [lein-kibit "0.1.5"]
+                       [lein-environ "1.1.0"]
+                       [io.aviso/pretty "0.1.34"]
+                       [lein-ancient "0.6.14"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.13"]
+                       [io.aviso/pretty "0.1.34"]]}}
