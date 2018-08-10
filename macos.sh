acd2312d99db25cb68c4a772c3790499ed087fff
Joaquim Torres <quimrstorres@gmail.com>
2018-08-10 15:44:34 +0200

    Comment line to disable local time machine backup

--- macos.sh
+++ macos.sh
@@ -118,8 +118,9 @@ defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
 # Prevent Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
+# TODO: Remove this completely? This no longer works on High Sierra
 # Disable local Time Machine backups
-hash tmutil &> /dev/null && sudo tmutil disablelocal
+# hash tmutil &> /dev/null && sudo tmutil disablelocal
 
 ###############################################################################
 # Activity Monitor                                                            #
