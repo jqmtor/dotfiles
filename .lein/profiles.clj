6857575e1783ee40c723447f48eb7fe0324a5349
Joaquim Torres <jqmtor@protonmail.com>
2021-06-21 11:35:52 +0200

    Update lein dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,8 @@
-{:user {:plugins             [[refactor-nrepl "2.5.0"]
-                              [cider/cider-nrepl "0.25.4"]
-                              [com.billpiel/sayid "0.0.17"]
-                              [lein-kibit "0.1.6"]
-                              [s3-wagon-private "1.3.1" :upgrade false]
-                              [lein-ancient "0.6.15"]]
+{:user {:plugins             [[refactor-nrepl "2.5.1"]
+                              [cider/cider-nrepl "0.25.11"]
+                              [com.billpiel/sayid "0.1.0"]
+                              [s3-wagon-private "1.3.4" :upgrade false]
+                              [lein-ancient "1.0.0-RC3"]]
         :injections          [(require 'nu)]
         :repositories        [["central"  {:url "https://repo1.maven.org/maven2/"
                                            :snapshots false}]
@@ -11,15 +10,10 @@
                               ["nu-maven" {:url "s3p://nu-maven/releases/"
                                            :region "sa-east-1"}]]
         :plugin-repositories [["nu-maven" {:url "s3p://nu-maven/releases/"}]]
-        :dependencies        [[cljdev "0.8.0"]]}
-
- :pretty {:plugins      [[io.aviso/pretty "0.1.37"]]
-          :dependencies [[io.aviso/pretty "0.1.37"]]
-          :middleware   [io.aviso.lein-pretty/inject]}
-
- :repl {:plugins      [[cider/cider-nrepl "0.25.4"]
-                       [refactor-nrepl "2.5.0"]]
-        :dependencies [[nrepl "0.8.2"]
+        :dependencies        [[cljdev "0.10.0"]]}
+ :repl {:plugins      [[cider/cider-nrepl "0.25.11"]
+                       [refactor-nrepl "2.5.1"]]
+        :dependencies [[nrepl "0.8.3"]
                        [mvxcvi/puget "1.3.1"]]
         :repl-options {:timeout 120000}}}
 
