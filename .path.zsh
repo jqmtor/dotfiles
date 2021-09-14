9c05ea59d7d85d5b6097a0cc1146b3a97b8dcd6e
Joaquim Torres <jqmtor@protonmail.com>
2021-09-14 07:03:46 +0200

    Reintroduce find utils with the non-prefixed name

--- .path.zsh
+++ .path.zsh
@@ -15,5 +15,5 @@ export PATH="$PATH:$HOME/.git_subcommands"
 # Add GNU utilities to path to use their original names (and not the -g prefix)
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-# export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
 
